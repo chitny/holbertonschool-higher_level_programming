eab1f8bbf4b0e033e503f96e060c14f612f6bbfb
Ignacio Chitnisky <elinmortal@gmail.com>
2021-04-27 15:11:11 +0000

    fixing pep8 in 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     result = ""
     for ch in str:
-        if ord(ch) >= 97 and ord(ch) <=122:
+        if ord(ch) >= 97 and ord(ch) <= 122:
             result += chr(ord(ch)-32)
         else:
             result += chr(ord(ch))
