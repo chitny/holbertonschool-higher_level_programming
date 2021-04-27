737cb529deb6b0418aea35ca8cb6b403284ace62
Ignacio Chitnisky <elinmortal@gmail.com>
2021-04-27 16:45:20 +0000

    fixing the new betty pep8

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,8 +2,7 @@
 def remove_char_at(str, n):
     ch = len(str)
     ch2 = ""
-    for x in range (0, ch):
+    for x in range(0, ch):
         if x != n:
-            ch2= ch2+str[x]
+            ch2 = ch2+str[x]
     return ch2
-    
