6ae3d35b3a30c9e5487f3dfe167e9c998ad25e0d
Ignacio Chitnisky <elinmortal@gmail.com>
2021-04-27 15:36:01 +0000

    fixing pep8 the new betty

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for x in range(122, 96, -1):
     if x % 2 == 0:
-        x= chr(x)
+        x = chr(x)
     else:
-        x=chr(x).upper()
+        x = chr(x).upper()
     print("{}".format(x), end="")
