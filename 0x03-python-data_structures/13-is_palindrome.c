7d54dcf2322a0f3ba6e2b2c9e805724866caef41
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-04 20:21:00 +0000

    changing 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -36,6 +36,22 @@ int is_palindrome(listint_t **head)
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
+	while (1)
+	{
+		fast = fast->next->next;
+		if (!fast)
+		{
+			dup = slow->next;
+			break;
+		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
+	}
+
 	reverse_listint(&dup);
 
 	while (dup && tmp)
