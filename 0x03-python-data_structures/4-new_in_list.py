d060eaa24a194bb99a5a7f5cdba6b220ede33ee4
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-04 16:18:14 +0000

    changing way of if

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list):
-        return my_list
-    else:
+    largo = len(my_list)
+    if idx in range(largo):
         new_list[idx] = element
         return new_list
+    else:
+        return my_list
