a5af08449b333678237c2f11b241e277287ceb60
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-04 15:38:26 +0000

    first five exercises

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    num=len(my_list)
+    for i in my_list:
+        print("{}".format(num))
+        num=num-1
+        
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+
+    my_new_string = ""
+    for i in my_string:
+        if i == "c" or i == "C":
+            i = ""
+
+        my_new_string = my_new_string + i
+    return my_new_string
