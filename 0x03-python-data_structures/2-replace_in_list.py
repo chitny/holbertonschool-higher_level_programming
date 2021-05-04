c5b75229a640b673bc397b11f2058a1c50f33fca
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-04 15:52:08 +0000

    another way to do the 2nd

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     largo = len(my_list)
     if idx in range(largo):
-        return my_list[idx]
-    else:
         my_list[idx] = element
         return my_list
+    else:
+        return
