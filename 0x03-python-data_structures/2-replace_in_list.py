0fa0684c0113659dbc2dfba8008845604028d96f
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-04 15:53:38 +0000

    forget the #

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     largo = len(my_list)
     if idx in range(largo):
         my_list[idx] = element
         return my_list
     else:
-        return
+        return my_list
