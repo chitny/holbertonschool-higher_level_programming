5df4087ba9e1f0276375aba45b2a4cd5b466e67c
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-04 19:14:22 +0000

    fixing el 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     result=()
-    if len(sentence)== 0:
+    if len(sentence) == 0:
         result = 0, "None"
     else:
         result = len(sentence), sentence[0]
