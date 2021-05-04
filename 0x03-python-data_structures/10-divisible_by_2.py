ee7fe90c54290c8b4aaff668885bc0871db3af6a
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-04 19:37:42 +0000

    adding 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,7 @@
 def divisible_by_2(my_list=[]):
     for i in len(my_list):
         divis = my_list[i] % 10
-        if divis == 0 or divis == 2 or divis == 4 
-            or divis == 6 or divis == 8
+        if divis == 0 or divis == 2 or divis == 4 or divis == 6 or divis == 8:
             return True
         else:
             return False
