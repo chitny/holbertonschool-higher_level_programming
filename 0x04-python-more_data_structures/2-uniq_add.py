a63e5ea44bd3e4a137c46985fe99d06eb2661770
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-06 15:44:17 +0000

    fixing el 8

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list=list(dict.fromkeys(my_list))
+    new_list = list(dict.fromkeys(my_list))
     x = 0
     for i in range(len(new_list)):
-        x= x + new_list[i]
+        x = x + new_list[i]
     return x
