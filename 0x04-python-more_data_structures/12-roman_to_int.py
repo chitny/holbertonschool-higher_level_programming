c55accc12762f05a7d31a3044dab03c82b7b5bfc
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-06 17:51:18 +0000

    fixing 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,18 +10,18 @@ def roman_to_int(roman_string):
 
     if len(roman_string) == 1:
         return roman_dict.get(roman_string)
-    else:
-        for i in range(len(roman_string)):
-            if i + 1 == len(roman_string):
-                x = roman_string[i]
-                z = roman_dict.get(x)
-                result = result + z
-            elif roman_dict.get(roman_string[i]) >= roman_dict.get(roman_string[i + 1]):
-                x = roman_string[i]
-                z = roman_dict.get(x)
-                result = result + z
-            else:
-                x = roman_string[i]
-                z = roman_dict.get(x)
-                result = result - z
+
+    for i in range(len(roman_string)):
+        if i + 1 == len(roman_string):
+            x = roman_string[i]
+            z = roman_dict.get(x)
+            result = result + z
+        elif roman_dict.get(roman_string[i]) >= roman_dict.get(roman_string[i + 1]):
+            x = roman_string[i]
+            z = roman_dict.get(x)
+            result = result + z
+        else:
+            x = roman_string[i]
+            z = roman_dict.get(x)
+            result = result - z
     return result
