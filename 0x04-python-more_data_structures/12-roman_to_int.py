843d9ccd82ca2fdfb0329bda11c85221fbd93ada
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-06 17:16:57 +0000

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10,
+                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+
+    for i in range(len(roman_string)):
+        x = roman_string[i]
+        z = roman_dict.get(x)
+        result = result + z
+    return result
