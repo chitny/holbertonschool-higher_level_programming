704b812f3763e42154f6faa0d8111ecd330f5077
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-06 17:56:27 +0000

    fixing 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10,
-                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
     result = 0
 
     if type(roman_string) is not str or roman_string is None:
