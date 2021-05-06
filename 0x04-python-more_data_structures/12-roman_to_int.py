f32f3ba71d2ff9971856a8b8ec97b9defc3089a4
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-06 17:59:40 +0000

    fixing el 8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
+    rom = {'I': 1, 'V': 5, 'X': 10,
+           'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
 
     if type(roman_string) is not str or roman_string is None:
         return 0
 
     if len(roman_string) == 1:
-        return roman_dict.get(roman_string)
+        return rom.get(roman_string)
 
     for i in range(len(roman_string)):
         if i + 1 == len(roman_string):
-            result += roman_dict. get(roman_string[i])
-        elif roman_dict.get(roman_string[i]) >= roman_dict.get(roman_string[i + 1]):
-            result += roman_dict. get(roman_string[i])
+            x = roman_string[i]
+            z = rom.get(x)
+            result = result + z
+        elif rom.get(roman_string[i]) >= rom.get(roman_string[i + 1]):
+            x = roman_string[i]
+            z = rom.get(x)
+            result = result + z
         else:
-            result -= roman_dict. get(roman_string[i])
+            x = roman_string[i]
+            z = rom.get(x)
+            result = result - z
     return result
