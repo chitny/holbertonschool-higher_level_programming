c3e996453b70ce60e5057d9d7aca12733debca15
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-06 16:45:27 +0000

    fixing 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,9 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+    valor = 0
+    for i, j, in a_dictionary.items():
+        if j > valor:
+            valor = j
+            nombre = i
+    return nombre
