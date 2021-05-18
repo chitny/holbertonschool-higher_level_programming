fbe48420cf8d2e18b8b4e8785e1d9d909e040b41
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-18 00:31:08 +0000

    fixing 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,6 +6,5 @@ def safe_print_division(a, b):
         print("Inside result: {}".format(result))
     except ZeroDivisionError:
         print("Inside result: None")
-        return None
     finally:
         return result
