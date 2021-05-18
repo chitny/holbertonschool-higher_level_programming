0fd04c9ef0629446d9665ad6bc29d5f863b03dc7
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-18 19:33:07 +0000

    adding 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class Square."""
+
+
+class Square():
+    """Represents an square."""
+
+    def __init__(self, size=0):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        elif type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
