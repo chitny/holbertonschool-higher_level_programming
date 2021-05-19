91424ac5437e881b44900f32f03514190389f161
Ignacio Chitnisky <elinmortal@gmail.com>
2021-05-19 01:12:10 +0000

    fixing el 8

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -25,6 +25,7 @@ class Square():
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     @property
     def position(self):
         return self.__position
@@ -37,7 +38,7 @@ class Square():
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value    
+        self.__position = value
 
     def my_print(self):
         if self.__size == 0:
