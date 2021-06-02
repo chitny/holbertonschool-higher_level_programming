4ac321694f8986d596b6463664b60b41b37b1760
Ignacio Chitnisky <elinmortal@gmail.com>
2021-06-02 00:54:48 +0000

    trying fix 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -8,13 +8,16 @@ class Rectangle(BaseGeometry):
     """Description of the class"""
 
     def __init__(self, width, height):
+        """never forget description"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area():
+        """never forget description"""
         return self.__width * self.__height
 
     def __str__(self):
-        return ("[{}] {}/{}".format(self.__class__, self.__width, self.__height))
+        """never forget description"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
