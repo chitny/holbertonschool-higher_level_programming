c1ed834913df0c579245a257fc4e601f20594d8e
Ignacio Chitnisky <elinmortal@gmail.com>
2021-06-02 21:11:48 +0000

    trying another way 5, 6 and 7

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,5 +12,6 @@ def save_to_json_file(my_obj, filename):
     """descripcion de la funcion,
     lo que hace y para que sirve
     """
-    with open(filename, mode="w+") as myfile:
+    with open(filename, mode="w+", encoding="utf-8") as myfile:
         json.dump(my_obj, myfile)
+    return
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -11,5 +11,5 @@ def load_from_json_file(filename):
     """descripcion de la funcion,
     lo que hace y para que sirve
     """
-    with open(filename, mode="r") as myfile:
-        return json.load(myfile)
+    with open(filename, mode="r", encoding="utf-8") as myfile:
+        return (json.load(myfile))
