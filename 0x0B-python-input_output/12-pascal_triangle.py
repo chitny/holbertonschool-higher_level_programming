122f72147a490c62b4a578342187fb07d15b64ea
Ignacio Chitnisky <elinmortal@gmail.com>
2021-06-02 23:53:04 +0000

    fixing pepocho

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,29 +11,28 @@ def pascal_triangle(n):
     """
     newlist = []
     actlist = [1]
-   
 
     if n <= 0:
         return []
 
-    for i in range (0, n):
+    for i in range(0, n):
 
         if i == 0:
             newlist.append([1])
         else:
             tmplist = []
 
-            for j in range (0, i+1):
+            for j in range(0, i+1):
                 add1 = 0
-    
+
                 if j > 0:
                     add1 = actlist[j-1]
 
-                add2 = 0    
+                add2 = 0
 
                 if j < len(actlist):
                     add2 = actlist[j]
-                
+
                 tmplist.append(add1+add2)
             actlist = tmplist
             newlist.append(actlist)
