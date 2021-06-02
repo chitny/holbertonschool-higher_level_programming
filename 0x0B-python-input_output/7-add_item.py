405632a646846a6c623c50858c801711f8686192
Ignacio Chitnisky <elinmortal@gmail.com>
2021-06-02 21:14:53 +0000

    check 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """
-function that reads a text file (UTF8) and prints it to stdout
 """
-
 
 import json
 from sys import argv
 from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+file_name = "add_item.json"
 
-
-obj = []
-
-if (path.exists('add_item.json')):
-    obj = load_from_json_file('add_item.json')
+if (path.exists(file_name)):
+    obj = load_from_json_file(file_name)
+else:
+    obj = []
 
 for i in range(1, len(argv)):
     obj.append(argv[i])
-save_to_json_file(obj, 'add_item.json')
+save_to_json_file(obj, file_name)
