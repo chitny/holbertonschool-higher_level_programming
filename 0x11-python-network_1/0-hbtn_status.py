ee6150f11c0b2f3a8842c1b6342f96878e3de8e9
Ignacio Chitnisky <elinmortal@gmail.com>
2022-01-09 04:15:23 +0000

    changing way to import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """0. What's my status? #0"""
+import urllib.request
 
 
 if __name__ == "__main__":
-    import urllib.request
-
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
