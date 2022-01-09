85ba48d619dfd93ddac27608481bc1d7290b8d53
Ignacio Chitnisky <elinmortal@gmail.com>
2022-01-09 02:46:30 +0000

    changing way to put a tab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
+        print('\N{tab}- type: {}'.format(type(html)))
+        print('\N{tab}- content: {}'.format(html))
+        print('\N{tab}- utf8 content: {}'.format(html.decode("utf-8", "replace")))
